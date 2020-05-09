b393604e1e43eb1988026b1cac14943f254def3b
awave1 <artgolovin96@gmail.com>
2020-05-09 15:20:01 -0600

    ✨ Feat: remove `Bio.js` and slightly update the page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,64 @@
 import React from 'react';
+import styled from 'styled-components';
 import Layout from '../components/layout';
-import Bio from '../components/Bio';
+
+const Wrapper = styled.div`
+  align-self: center;
+  margin: 35px;
+  width: 100%;
+
+  @media screen and (max-width: 360px) {
+    margin: 15px;
+  }
+`;
+
+const A = styled.a`
+  color: black;
+  text-decoration: none;
+  padding: 5px;
+  transition: 200ms all cubic-bezier(0.075, 0.82, 0.165, 1);
+
+  &:hover {
+    background-color: black;
+    color: white;
+    border-radius: 5px;
+  }
+`;
+
+const List = styled.ul`
+  list-style: none;
+  padding: 0;
+`;
+
+const ListLink = props => (
+  <li style={{ margin: '5px 0', padding: '5px 0' }}>
+    <A href={props.to}>{props.children}</A>
+  </li>
+);
 
 const IndexPage = () => (
   <Layout>
-    <Bio />
+    <Wrapper>
+      <h1>
+        Artem Golovin{' '}
+        <span role="img" aria-label="wave">
+          🌊
+        </span>
+      </h1>
+      <h3>Software Developer</h3>
+      <p>
+        I enjoy building things, taking pictures, and making music
+      </p>
+
+      <List>
+        <ListLink to="https://github.com/awave1">/github</ListLink>
+        <ListLink to="https://www.linkedin.com/in/artem-golovin-749793a5/">/linkedin</ListLink>
+        <ListLink to="https://twitter.com/awaveawave">/twitter</ListLink>
+        <ListLink to="https://soundcloud.com/awave1/tracks">
+          /soundclound
+        </ListLink>
+      </List>
+    </Wrapper>
   </Layout>
 );
 
